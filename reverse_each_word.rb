64c4f1ffb491608951eb0c23c0f8f0f65e1e2bb9
john L <johnleavell@gmail.com>
2019-11-16 12:59:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,13 @@
 
-def reverse_each_word(phrase)
-p "phrase".reverse
+# def reverse_each_word(phrase)
+# phrase.split.collect {|word| word.reverse}.join(")")
+#
+# end
+
+def reverse_each_word(sentence)
+
+string.map{|string|string.reverse}
+
 
 
 end
